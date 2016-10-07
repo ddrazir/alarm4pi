c8daf0b1ccc9f346d49fe4d40414a8e8b0ef4518
Drazir <drazir@hotmail.com>
2016-10-07 23:19:00 +0200

    set_timer() fn added although it will probably not be used because of interferences of timer with system fns execution

--- alarm4pi.c
+++ alarm4pi.c
@@ -13,11 +13,14 @@
 #include <syslog.h>
 #include <arpa/inet.h> //INET6_ADDRSTRLEN
 #include <signal.h>
+#include <sys/time.h>
 
 #include "bcm_gpio.h"
 #include "log_msgs.h"
 #include "port_mapping.h"
 #include "public_ip.h"
+
+#define SENSOR_POLLING_PERIOD_SEC 1
 
 pid_t Child_process_id[2]={-1,-1}; // Initialize to -1 in order no to send signals if no child process created
 char * const Capture_exec_args[]={"nc", "-l", "-p", "8080", "-v", "-v", NULL};
@@ -25,6 +28,7 @@ char * const Web_server_exec_args[]={"nc", "-l", "-p", "8008", "-v", "-v", NULL}
 
 static volatile sig_atomic_t Exit_daemon_loop=0; // When Break is pressed this var is set to 1 by the Control Handler to exit any time consuming loop
 
+/*
 static int daemonize(char *working_dir)
   {
    int ret_error;
@@ -41,7 +45,7 @@ static int daemonize(char *working_dir)
       if(setsid() != -1) // creates a session and sets the process group ID
         {
          // Catch, ignore and handle signals
-         //TODO: Implement a working signal handler */
+         // TODO: Implement a working signal handler
          signal(SIGCHLD, SIG_IGN);
          signal(SIGHUP, SIG_IGN);
 
@@ -98,7 +102,7 @@ static int daemonize(char *working_dir)
       
    return(ret_error);
   }
-
+*/
 void kill_processes(pid_t *process_ids, size_t n_processes)
   {
    int n_child;
@@ -107,6 +111,11 @@ void kill_processes(pid_t *process_ids, size_t n_processes)
          kill(process_ids[n_child], SIGTERM);   
   }
 
+// This function blocks until a list of processes terminate or timeout
+// process_ids is a pointer to the PID list of length n_processes
+// Returns 0 on success (all child processes have finished) or an errno
+// code if on error or timeout
+// Warning: This function stops the system timer
 int wait_processes(pid_t *process_ids, size_t n_processes, int wait_timeout)
   {
    int ret_error;
@@ -120,7 +129,6 @@ int wait_processes(pid_t *process_ids, size_t n_processes, int wait_timeout)
       n_remaining_procs=0;
       alarm(wait_timeout);
       wait_ret=waitpid(0, NULL, 0); // wait for any child process whose process group ID is equal to that of the calling process.
-      log_printf("waitpid: ret: %i\n",wait_ret);
       if(wait_ret != -1) // Valid PID returned
         {
          int n_child;
@@ -129,7 +137,10 @@ int wait_processes(pid_t *process_ids, size_t n_processes, int wait_timeout)
             if(process_ids[n_child] != -1) // A process is remaining in the list
               {
                if(process_ids[n_child] == wait_ret) // Is the process that has just died?
+                 {
+                  log_printf("Child process with PID: %i terminated.\n", wait_ret);
                   process_ids[n_child] = -1; // Mark process as dead
+                }
                else
                   n_remaining_procs++; // We still have to wait for his process
               }
@@ -230,49 +241,93 @@ static int run_background_command(pid_t *new_proc_id, const char *exec_filename,
    return(ret);
   }
 
+// Configure the system real-time timer to send a SIGALRM signal to the current process
+// SIGALRM must be handled before calling this function
+// this signal will be send each interval_sec seconds
+// If interval_sec is negative, the timer is stopped
+// The function returns 0 on success, or a errno error code on error
+int configure_timer(float interval_sec)
+  {
+   int ret_error;
+   struct itimerval timer_conf;
+
+   if(interval_sec < 0) // Disable the timer
+     {
+      // Set the timer to zero: A timer whose it_value is zero or the timer expires
+      // and it_interval is zero stops
+      timer_conf.it_value.tv_sec = 0;
+      timer_conf.it_value.tv_usec = 0;
+      timer_conf.it_interval.tv_sec = 0;
+      timer_conf.it_interval.tv_usec = 0;
+     }
+   else
+     {
+      // Configure the timer to expire (ring) after 250 ms the first time
+      timer_conf.it_value.tv_sec = 0;
+      timer_conf.it_value.tv_usec = 250000;
+      // and to expire again every interval_sec seconds
+      timer_conf.it_interval.tv_sec = (time_t)interval_sec;
+      timer_conf.it_interval.tv_usec = (suseconds_t)((interval_sec-timer_conf.it_interval.tv_sec)*1.0e6);
+     }
+
+   // Start a real time timer, which deliver a SIGALARM
+   if(setitimer (ITIMER_REAL, &timer_conf, NULL) == 0)
+     {
+      log_printf("Sensor polling (timer) set to %lis and %lius\n", timer_conf.it_interval.tv_sec, timer_conf.it_interval.tv_usec);
+      ret_error=0; // Timer set successfully
+     }
+   else
+     {
+      ret_error=errno;
+      log_printf("Error setting timer: errno %i: %s\n", errno, strerror(errno));
+     }
+   return(ret_error);
+  }
 
 int main(int argc, char *argv[])
   {
-   int repeat = 10;
    int main_err;
-   int value;
    char wan_address[INET6_ADDRSTRLEN];
    pid_t capture_proc, web_server_proc;
    
 
-   //main_err=daemonize("/");
+   // main_err=daemonize("/"); // Custom fn, but it causes problems when waiting for child processes
    main_err=daemon(0,0);
    if(main_err == 0)
      {
-      //config_UPNP(NULL);
-      //if(get_public_ip(wan_address)==0)
-      //   printf("Public IP: %s\n", wan_address);
-      //return(0);
-      ///syslog(LOG_NOTICE, "iAlarm daemon started.");
+      syslog(LOG_NOTICE, "iAlarm daemon started.");
       
       if(open_log_files())
          syslog(LOG_WARNING, "Error creating log files.");
       
       set_signal_handler();
 
+      //config_UPNP(NULL);
+
+
+      if(get_public_ip(wan_address)==0)
+         log_printf("Public IP address: %s\n", wan_address);
+
       if(run_background_command(&capture_proc, Capture_exec_args[0], Capture_exec_args)==0)
         {
          Child_process_id[0]=capture_proc;
+         log_printf("Child process %s executed\n", Capture_exec_args[0]);
          if(run_background_command(&web_server_proc, Web_server_exec_args[0], Web_server_exec_args)==0)
            {
             Child_process_id[1]=web_server_proc;
 
-            log_printf("Waiting for child processes\n");
-
-
-            wait_processes(Child_process_id, sizeof(Child_process_id)/sizeof(pid_t), 5);
+            log_printf("Child process %s executed\n", Web_server_exec_args[0]);
           }
         }
-        
+
+      configure_timer(SENSOR_POLLING_PERIOD_SEC); // Activate timer
    /*
       main_err=export_gpios();
       if(main_err==0)
         {
+         int value;
+         int repeat = 10;
+
          main_err=configure_gpios();
          if(main_err==0)
            {
@@ -299,8 +354,20 @@ int main(int argc, char *argv[])
          unexport_gpios();
         }
         */
-      close_log_files();
 
+      sleep(10);
+
+      log_printf("Waiting for child processes to finish\n");
+
+      configure_timer(-1); // Stop timer
+
+      // Wait until created process terminate or time out
+      // The system timer (used for polling) is stopped by this function
+      // 5
+      wait_processes(Child_process_id, sizeof(Child_process_id)/sizeof(pid_t), 0);
+
+
+      close_log_files();
      }
    return(main_err);
   }
